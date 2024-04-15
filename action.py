737b455a1213a7cb70f1f3a8591c94cc7ccf20a2
lowo <870050851@qq.com>
2024-04-15 13:30:45 +0800

    Update action.py

--- action.py
+++ action.py
@@ -407,23 +407,23 @@ class Action:
         #             selected_pai = pai
         #             break
 
-        # # 用此方法随机，掉分，若注释此段则一直一选
-        # if ai_pais[0][1] < 0.9:
-        #     # 改变一选的概率: 0~1.0时，越接近0非一选的概率越大 ; 大于1.0时，越大一选的概率越大(一般取1~5)
-        #     ex_power = 0.8
-        #     # 筛选出概率大于0.1的元组
-        #     filtered_list = [(pai, prob ** ex_power) for pai, prob in ai_pais if prob > 0.1]
-        #     # 计算总权重
-        #     total_weight = sum(prob for _, prob in filtered_list)
-        #     # 随机选择一个操作
-        #     random_value = random.uniform(0, total_weight)
-        #     cumulative_weight = 0
-        #     selected_pai = None
-        #     for pai, prob in filtered_list:
-        #         cumulative_weight += prob
-        #         if random_value <= cumulative_weight:
-        #             selected_pai = pai
-        #             break
+        # 用此方法随机，掉分，若注释此段则一直一选
+        if ai_pais[0][1] < 0.9:
+            # 改变一选的概率: 0~1.0时，越接近0非一选的概率越大 ; 大于1.0时，越大一选的概率越大(一般取1~5)
+            ex_power = 0.4
+            # 筛选出概率大于0.1的元组
+            filtered_list = [(pai, prob ** ex_power) for pai, prob in ai_pais if prob > 0.1]
+            # 计算总权重
+            total_weight = sum(prob for _, prob in filtered_list)
+            # 随机选择一个操作
+            random_value = random.uniform(0, total_weight)
+            cumulative_weight = 0
+            selected_pai = None
+            for pai, prob in filtered_list:
+                cumulative_weight += prob
+                if random_value <= cumulative_weight:
+                    selected_pai = pai
+                    break
 
         mjai_msg['pai'] = selected_pai
         dahai = mjai_msg['pai']
