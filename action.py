82aacb192c1e80e2e17ddadc30460901ef55dd24
lowo <870050851@qq.com>
2024-04-16 21:30:29 +0800

    Update action.py

--- action.py
+++ action.py
@@ -410,8 +410,8 @@ class Action:
         if ai_pais[0][1] < 0.85:
             # 改变一选的概率: 0~1.0时，越接近0非一选的概率越大 ; 大于1.0时，越大一选的概率越大(一般取1~5)
             ex_power = 0.4 # 取0.45时重合率71%~81%,分数92~95;取0.4时重合率68%~75%,分数90~94;取0.5时重合率84%~85.5%,分数94~95
-            # 筛选出概率大于0.1的元组
-            filtered_list = [(pai, prob ** ex_power) for pai, prob in ai_pais if prob > 0.1]
+            # 筛选出概率大于0.15的元组
+            filtered_list = [(pai, prob ** ex_power) for pai, prob in ai_pais if prob > 0.13]
             # 计算总权重
             total_weight = sum(prob for _, prob in filtered_list)
             # 随机选择一个操作
